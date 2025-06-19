82c1ca24767603bbbec143cdf9bda64471138409
Biradar09 <113668904+Biradar09@users.noreply.github.com>
2025-06-19 21:45:59 +0530

    Update AppTest.java

--- src/test/com/example/HelloApp/AppTest.java
+++ src/test/com/example/HelloApp/AppTest.java
@@ -1,1 +1,12 @@
+package com.example.HelloApp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
 
